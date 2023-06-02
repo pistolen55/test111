26ff79a1bddb4de41b2556bd074d053104298463
pistolen55 <133331299+pistolen55@users.noreply.github.com>
2023-06-02 11:16:38 +0900

    Create a.py

---
+++ a.py
@@ -0,0 +1,11 @@
+import pyson
+
+def print_specific_string():
+    # 文字列を設定します
+    specific_string = "特定の文字列"
+
+    # 文字列を出力します
+    pyson.output(specific_string)
+
+# 関数を呼び出して特定の文字列を出力します
+print_specific_string()
